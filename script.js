594e82667dbf5f09272eeaf413d0828aa09076f7
GMZwoods <francois.xdg@gmail.com>
2024-02-04 20:55:41 -0500

    afficher le slider correction de certains input validation des numeros , validations des pseudo

--- script.js
+++ script.js
@@ -1,25 +1,40 @@
 /*
 * AFFICHER LES PATATES
-* */
-
-
-let slider = document.getElementById("nbPatates");
-let output = document.getElementById("PatatesQt");
-
-output.innerHTML =slider.value
-/*
-output.innerHTML = slider.value;
-
-slider.oninput = function () {
-        output.innerHTML =this.value
-    }
+*
 */
-function ValidationNumeroTel()
+function Afficherpetake() {
+    let x = document.getElementById("NbPatates").value;
+    document.getElementById("PatatesQt").innerHTML = x;
+}
+
+//Verifier que le nombre de pommes est < que patates
+const QTpommes = document.getElementById("NbPommes").value;
+const QTPatates = document.getElementById("NbPatates").value;
+function PlusDePetakes()
 {
-    let SelectOntario = document.getElementById("provinceCanada").value
-    if(SelectOntario.value !== "Ontario")
+    if(QTpommes > QTPatates)
     {
-        alert("votre numéro doit commence par 613-xxx-xxxx")
+        alert("You MUST have more Taters");
 
     }
 }
+
+
+
+
+
+function Validation()
+{
+//ne fonctionne pas encore
+    let NumeroUtilisateur = document.getElementById("téléphoneUtilisateur").value;
+    let Resultat = NumeroUtilisateur.startsWith("819");
+    let ChoixProvince = document.getElementById("provinceCanada").value;
+    if (ChoixProvince === "Quebec")
+    {
+        if (Resultat === false)
+        {
+            alert("Vous devez avoir 819")
+        }else {return true}
+    }
+}
+
